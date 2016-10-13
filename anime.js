ce55986dc7ac3eb4ece6e9f77e47fe52722a467a
Asrul Hanafi <hanafi.asrul@gmail.com>
2016-10-13 14:21:40 +0700

    Update detail anime and add search

--- anime.js
+++ anime.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var fuse = require('fuse.js');
 
 // Config
 var port = process.env.PORT | 8888;
@@ -14,8 +15,7 @@ var source = 'http://animeindo.web.id';
 // Route
 app.get('/', greeting);
 app.get('/anime', listAnime);
-app.get('/anime/:name', watchAnime);
-app.get('/anime/detail/:name', detailAnime);
+app.get('/anime/:name/:ep', detailAnime);
 app.post('/anime/search', searchAnime);
 
 // Behavior
@@ -54,7 +54,7 @@ function listAnime(req, res) {
 		if (!error) {
 			var $ = cheerio.load(html);
 			var datas = [];
-			var title, thumbnail, uploaded, detail, image, url;
+			var title, thumbnail, uploaded, detail, image, url, episode;
 			var data = $('#episodes').children('.episode');
 
 			data.each(function(index, el) {
@@ -65,12 +65,16 @@ function listAnime(req, res) {
 				uploaded = detail.children('div').children('.episode-meta').text();
 				thumbnail = image.children('.primary').attr('src');
 				url = image.attr('href');
+				if (url) {
+					episode = url.substr(url.indexOf('-subtitle-indonesia') - 2, 2);
+					url = url.substr(0, url.indexOf('-subtitle-indonesia') - 3);
+				}
 
 				datas.push({
 					title: title ? title : 'No title',
 					uploaded: uploaded ? uploaded : false,
 					thumbnail: thumbnail ? thumbnail : 'default',
-					url: url ? baseUrl + url.split('/')[5] : false
+					url: url ? baseUrl + url.split('/')[5] + '/' + parseInt(episode): false
 				});
 			});
 		    res.status(200).json(datas);
@@ -86,86 +90,9 @@ function listAnime(req, res) {
  * @param  {Object} res 
  * @return {Void}
  */
-function watchAnime(req, res) {
-	var name = req.params.name;
-	var host = req.get('host');
-	var baseUrl = req.protocol + '://' + host + '/anime/';
-
-	if (name) {
-		var url = source + '/' + name;
-		request(url, function(error, response, html) {
-			if (!error) {
-				var $ = cheerio.load(html);
-				var title, stream, streamStart, streamStop, prev, index, next;
-				var body = $('.bannertit');
-				var video = $('.player-area');
-				var options = $('#episode-options');
-
-				title = name.replace('-subtitle-indonesia', '').replaceAll('-', ' ');
-				stream = video.children().children().children().children('div').children('script').text();
-				streamStart = stream.indexOf('http://www.blogger.com/video-play.mp4');
-				streamStop = stream.indexOf('"image"') - streamStart;
-				stream = stream.substr(streamStart, streamStop - 10);
-
-				prev = options.children('.ep-prev').children('a').attr('href');
-				index = options.children('.ep-more').children('a').attr('href');
-				next = options.children('.ep-next').children('a').attr('href');
-
-				var baseResult = {
-			    	title: title, 
-			    	stram: stream,
-			    	nav: {
-			    		prev: prev ? baseUrl + prev.split('/')[5] : false,
-			    		index: index ? baseUrl + 'detail/' + index.split('/')[4] : false,
-			    		next: next ? baseUrl + next.split('/')[5] : false
-			    	}
-			    };
-
-				// getRating(res, name, baseResult); // Rating
-				res.status(200).json(baseResult);
-			} else {
-			    res.status(403).json({status: false, message: 'Page error!'});
-			}
-		});
-	} else {
-	    res.status(500).json({status: false, message: 'Anime not found!'});
-	}
-}
-
-/**
- * Get rating (Too slow not recomended)
- * @param  {Object} res
- * @param  {String} name
- * @param  {Object} baseResult
- * @return {Void}
- */
-function getRating(res, name, baseResult) {
-	var searchAble = name.replace('-subtitle-indonesia', '').replaceAll('-', '+');
-	var urlRating = 'https://myanimelist.net/search/prefix.json?type=anime&keyword=' + searchAble + '&v=1';
-	request(urlRating, function(error, response, html) {
-		if (!error) {
-			var listResult = JSON.parse(response.body).categories[0].items;
-			if (listResult.length >= 1) {
-				baseResult.payload = listResult[0].payload;
-				baseResult.payload.name = listResult[0].name;
-				res.status(200).json(baseResult);
-			} else {
-				res.status(200).json(baseResult);
-			}
-		} else {
-			res.status(200).json(baseResult);
-		}
-	});
-}
-
-/**
- * View Anime detail
- * @param  {Object} req 
- * @param  {Object} res 
- * @return {Void}
- */
 function detailAnime(req, res) {
 	var name = req.params.name;
+	var ep = req.params.ep || '';
 	var host = req.get('host');
 	var baseUrl = req.protocol + '://' + host + '/anime/';
 
@@ -174,14 +101,28 @@ function detailAnime(req, res) {
 		request(url, function(error, response, html) {
 			if (!error) {
 				var $ = cheerio.load(html);
-				var title, thumbnail, description, summary;
+				var title, thumbnail, description, rating, summary;
 				var episodes = [];
-				var desc = $('.cat_box_desc').children('div').text().split('Sinopsis:');
 				var epList = $('.episode_list');
 				title = $('.amin_week_box_up1').text();
-				thumbnail = $('.cat_image').children().attr('href');
-				description = desc[0];
-				summary = desc[1];
+				thumbnail = $('.cat_image').children().attr('src');
+				description = $('.cat_box_desc').children('div').text();
+
+				var indexRating = description.indexOf('Rating');
+				indexRating = indexRating != -1 ? indexRating + 8 : description.indexOf('Skor') + 6;
+				rating = 0.00;
+				if (indexRating != -1) {
+					rating = description.substr(indexRating, 4);
+					rating = (parseFloat(rating) / 2).toFixed(2);
+					rating = parseFloat(rating);
+				}
+
+				var indexSummary = description.indexOf('Sinopsis');
+				summary = '';
+				if (indexSummary != -1) {
+					summary = description.substr(indexSummary + 9, description.length - indexSummary);
+				}
+
 				epList.each(function(index, el) {
 					episodes.push(baseUrl + $(this).children('a').attr('href').split('/')[5]);
 				});
@@ -189,8 +130,9 @@ function detailAnime(req, res) {
 				res.status(200).json({
 					title: title ? title : 'No title',
 					thumbnail: thumbnail ? thumbnail : 'default',
-					description: description,
-					summary: summary ? summary : '',
+					stream: ep ? episodes[episodes.length - ep] : episodes[0], // Sampai sini
+					rating: rating,
+					summary: summary,
 					episodes: episodes
 				});
 			} else {
@@ -202,14 +144,46 @@ function detailAnime(req, res) {
 	}
 }
 
+/**
+ * Search Anime
+ * @param  {Object} req 
+ * @param  {Object} res 
+ * @return {Void}
+ */
 function searchAnime(req, res) {
 	var search = req.body.filter || '';
-	var url = source + '/?s=' + search;
+	var url = source + '/anime-list-animeindo';
 	var host = req.get('host');
 	var baseUrl = req.protocol + '://' + host + '/anime/';
-	res.status(200).json({
-		status: search
-	});
+
+	if (search) {
+		request(url, function(error, response, html) {
+			if (!error) {
+				var $ = cheerio.load(html);
+				var anime = $('.amin_box_mid_link');
+				var listAnime = [];
+
+				anime.each(function(index, el) {
+					var link = $(this).children();
+					listAnime.push({
+						title: link.text(),
+						url: link.attr('href')
+					});
+				});
+
+				var initSearch = new fuse(listAnime, { keys: ["title"] });
+				var result = initSearch.search(search);
+
+				res.status(200).json({
+					result: result
+				});
+			} else {
+			    res.status(403).json({status: false, message: 'Page error!'});
+			}
+		});
+	} else {
+	    res.status(403).json({status: false, message: 'No keyword!'});
+	}
 }
 
 // Run
